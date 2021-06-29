1d598371ed64a571a8283dc5908897db33be592f
KayvanShah1 <shahkayvan28@gmail.com>
2021-06-30 00:23:47 +0530

    changes

--- src/thomasnet/metadata_scraper/thomasnet_metascraper.py
+++ src/thomasnet/metadata_scraper/thomasnet_metascraper.py
@@ -1,26 +1,46 @@
 import os, re, time
+import traceback
 
 import requests
 from bs4 import BeautifulSoup
 
 import numpy as np
 import pandas as pd
+import warnings
+warnings.simplefilter(action='ignore')
 
 from multiprocessing import Pool
 
 
 class ThomasnetMetaDataScraper:
-    def __init__(self):
+    def __init__(self, **kwargs):
+        self.config = kwargs.get('config')
         self.collected_data = []
+        self.list_of_payloads = []
+        self.BASE_URL = "https://www.thomasnet.com/nsearch.html"
 
     def generate_payload(self):
         pass
 
-    def extract_data():
+    @staticmethod
+    def extract_data(payload):
         pass
 
     def save_data(self):
-        pass
+        try:
+            pool = Pool(processes=25)
+            final_result = pool.map(self.extract_data, self.list_of_payloads)
+            for result in final_result:
+                if result["success"]:
+                    self.collected_data.extend(result["page_data"])
+        except Exception as e:
+            print(f"Error occurred. Closing scraping process.\n{str(e)}", traceback.print_exc())
+        finally:
+            self.save_data()
 
     def run(self):
         pass
+
+
+if __name__ =='__main__':
+    scraper = ThomasnetMetaDataScraper()
