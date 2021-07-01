8e0bc756173672b3c4632cfd6b7a6f020ff349c4
KayvanShah1 <shahkayvan28@gmail.com>
2021-07-01 22:41:13 +0530

    master harvester

--- src/thomasnet/scraper/thomasnet.py
+++ src/thomasnet/scraper/thomasnet.py
@@ -1,5 +1,4 @@
 import time
-import os
 import traceback
 from tqdm import tqdm
 
@@ -20,6 +19,7 @@ class ThomasnetScraper:
         self.ref_urls_df = pd.DataFrame(columns=self.data_columns)
         self.success_urls = pd.DataFrame(columns=self.data_columns)
         self.failed_urls = pd.DataFrame(columns=self.data_columns)
+        self.master_df = None
 
         self.collected_data = []
 
@@ -32,7 +32,10 @@ class ThomasnetScraper:
         return pd.DataFrame(scraping_list, columns=self.data_columns)
 
     def add_to_collected_data(self, page_data):
-        self.collected_data.append(page_data)
+        if type(page_data) == dict:
+            self.collected_data.append(page_data)
+        elif type(page_data) == list:
+            self.collected_data.extend(page_data)
 
     def add_url_to_success_list(self, company_id, url: str):
         self.success_urls = self.success_urls.append(
@@ -56,13 +59,143 @@ class ThomasnetScraper:
                 retry = retry + 1
                 print(f"Retrying {retry}/5 times...")
                 if retry == 5:
+                    print(f"Error requesting URL:{url}")
                     break
                 else:
                     time.sleep(5)
                     pass
 
-    def extract_data(self):
-        pass
+    def extract_data(self, row):
+        page_data = {
+            "company_id": row[0],
+            "company_name": "",
+            "company_type": "",
+            "company_url": "",
+            "location": "",
+            "telephone": "",
+            "product_description": "",
+            "about_company": "",
+            "product_types": "",
+            "all_product_services": "",
+            "link_prod_services": "",
+            "brands": "",
+            "link_all_brands": "",
+            "primary_company_type": "",
+            "additional_activities": "",
+            "key_personnel": "",
+            "social_media": "",
+            "other_locations": "",
+            "annual_revenue": "",
+            "year_founded": "",
+            "num_employees": "",
+            "url": row[1],
+        }
+        try:
+            soup = self.get_response(row[0])
+            gen_info = soup.find("div", {"class": "copro_naft"})
+            page_data["company_name"] = (
+                gen_info.find("div", {"class": "codetail"})
+                .find("h1")
+                .find("a")
+                .text.strip()
+            )
+            page_data["company_url"] = (
+                gen_info.find("div", {"class": "codetail"})
+                .find("h1")
+                .find("a")
+                .get("href")
+            )
+            page_data["company_type"] = (
+                gen_info.find("div", {"class": "codetail"})
+                .find("p")
+                .findAll("span")[2]
+                .text.strip()
+            )
+            page_data["location"] = (
+                gen_info.find("p", {"class": "addrline"}).text.split("|")[0].strip()
+            )
+            page_data["telephone"] = gen_info.find(
+                "a", {"data-conversion_action": "Call"}
+            ).get("href")
+
+            ################# Business Description ####################
+            business_desc = soup.find("div", {"id": "copro_description"})
+            page_data["product_description"] = business_desc.find(
+                "div", {"id": "copro_pdm"}
+            ).text.strip()
+            page_data["about_company"] = business_desc.find(
+                "div", {"id": "copro_about"}
+            ).text.strip()
+
+            ################# Products and Services/Brands ####################
+            prod_serv = soup.find("div", {"id": "copro_prodserv"})
+            try:
+                prod_cats = prod_serv.find("div", {"id": "copro_prodserv_cats"})
+                prod_types = prod_cats.findAll("div", {"class": "prodserv_group"})[0]
+                page_data["product_types"] = [
+                    i.text.strip() for i in prod_types.findAll("li")
+                ]
+                all_prodserv = prod_cats.findAll("div", {"class": "prodserv_group"})[1]
+                page_data["all_product_services"] = [
+                    i.text.strip() for i in all_prodserv.findAll("li")
+                ]
+                page_data[
+                    "link_prod_services"
+                ] = "https://www.thomasnet.com" + all_prodserv.find("a").get("href")
+            except Exception as e:
+                pass
+
+            try:
+                prod_brands = prod_serv.find("div", {"id": "copro_prodserv_brands"})
+                brands = prod_brands.find("div", {"class": "prodserv_group"})
+                page_data["brands"] = [i.text.strip() for i in brands.findAll("li")]
+                page_data[
+                    "link_all_brands"
+                ] = "https://www.thomasnet.com" + brands.find("a").get("href")
+            except Exception as e:
+                pass
+
+            ################# Business Details ####################
+            bus_det = soup.find("div", {"id": "copro_bizdetails"})
+            col1 = bus_det.find("div", {"class": "bdcol1"})
+            col1_ = col1.findAll("div", {"class": "bizdetail"})
+            for div in col1_:
+                label = div.find("div", {"class": "label"}).text.strip()
+                if label == "Primary Company Type:":
+                    page_data["primary_company_type"] = div.find("li").text.strip()
+                if label == "Additional Activities:":
+                    page_data["additional_activities"] = div.find("li").text.strip()
+                if label == "Key Personnel:":
+                    page_data["key_personnel"] = [
+                        i.text.strip() for i in div.findAll("li")
+                    ]
+                if label == "Locations:":
+                    page_data[
+                        "other_locations"
+                    ] = "https://www.thomasnet.com" + div.find("a").get("href")
+                if label == "Social:":
+                    page_data["social_media"] = [
+                        i.get("href") for i in div.findAll("a")
+                    ]
+
+            col2 = bus_det.find("div", {"class": "bdcol2"})
+            col22_ = col2.findAll("div", {"class": "bizdetail"})
+            for div in col22_:
+                if div.find("div", {"class": "label"}):
+                    label = div.find("div", {"class": "label"}).text.strip()
+                    if label == "Annual Sales:":
+                        page_data["annual_revenue"] = div.find("li").text.strip()
+                    if label == "No of Employees:":
+                        page_data["num_employees"] = div.find("li").text.strip()
+                    if label == "Year Founded:":
+                        page_data["year_founded"] = div.find("li").text.strip()
+
+            ################# Consolidation of Data ####################
+            self.add_to_collected_data(page_data)
+            self.add_url_to_success_list(row[0], row[1])
+        except Exception as e:
+            self.add_url_to_failed_list(row[0], row[1])
+            print(f"Error scraping page\n{e}\n{row[1]}")
 
     def load_data(
         self,
@@ -87,12 +220,14 @@ class ThomasnetScraper:
             print(f"Total successful urls: {self.success_urls.shape[0]}")
         except FileNotFoundError as e:
             print("Success URLs file not found:\n", e)
+            pass
 
         try:
             self.failed_urls = pd.read_csv(failed_urls_path)
             print(f"Total failed urls: {self.failed_urls.shape[0]}")
         except FileNotFoundError as e:
             print("Failed URLs file not found:\n", e)
+            pass
 
         self.scraping_list_df = self.get_scraping_list_df()
         print(f"Total urls to be tried: {self.scraping_list_df.shape[0]}")
@@ -103,10 +238,51 @@ class ThomasnetScraper:
         success_urls_path: str = "",
         failed_urls_path: str = "",
     ):
-        pass
+        try:
+            if self.master_df == None:
+                self.master_df = pd.DataFrame(self.collected_data)
+            else:
+                self.master_df = self.master_df.append(
+                    pd.DataFrame(self.collected_data), ignore_index=True
+                )
+            self.master_df.drop_duplicates(
+                subset=self.data_columns[0], keep="last", inplace=True
+            )
+            self.master_df.to_csv(master_data_path, index=False)
+        except Exception as e:
+            print(f"Error saving data: {e}")
+
+        try:
+            self.success_urls.to_csv(success_urls_path, index=False)
+            self.failed_urls.to_csv(failed_urls_path, index=False)
+        except Exception as e:
+            print(f"Error logging URLs: {e}")
 
     def run(self):
-        pass
+        try:
+            self.load_data(
+                reference_urls_path=self.config["paths"]["reference_url_path"],
+                master_data_path=self.config["paths"]["master_data_path"],
+                success_urls_path=self.config["paths"]["success_url_path"],
+                failed_urls_path=self.config["paths"]["failed_url_path"],
+            )
+
+            for _, row in tqdm(
+                self.scraping_list_df.iterrows(), total=self.scraping_list_df.shape[0]
+            ):
+                self.extract_data(row)
+
+        except Exception as e:
+            print(
+                traceback.print_exc(),
+                f"\nError occurred. Killing the scraping process:\n{e}",
+            )
+        finally:
+            self.save_data(
+                master_data_path=self.config["paths"]["master_data_path"],
+                success_urls_path=self.config["paths"]["success_url_path"],
+                failed_urls_path=self.config["paths"]["failed_url_path"],
+            )
 
 
 if __name__ == "__main__":
@@ -116,7 +292,7 @@ if __name__ == "__main__":
         "paths": {
             "saving_path": "data/hydraulic_cylinders/hydraulic_cylinders_suppliers_metadata.csv",
             "reference_url_path": "data/hydraulic_cylinders/hydraulic_cylinders_suppliers_urls.csv",
-            "sucess_url_path": "data/hydraulic_cylinders/sucess_url.csv",
+            "success_url_path": "data/hydraulic_cylinders/success_url.csv",
             "failed_url_path": "data/hydraulic_cylinders/failed_url.csv",
             "master_data_path": "data/hydraulic_cylinders/hydraulic_cylinders_master_data.csv",
             "cleaned_data_path": "data/hydraulic_cylinders/hydraulic_cylinders_clean_data.csv",
